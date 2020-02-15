863a7f22d6154a099ec74adf06198599ec10f284
Rosemberg Bezerra <rosemberg.bezerra@gmail.com>
2020-02-15 17:03:32 -0300

    Tela inicial

--- src/app/folder/folder.module.ts
+++ src/app/folder/folder.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { FolderPageRoutingModule } from './folder-routing.module';
 
 import { FolderPage } from './folder.page';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FolderPageRoutingModule
+    FolderPageRoutingModule,
+    RouterModule.forChild([{path: '', component: FolderPage}])
   ],
   declarations: [FolderPage]
 })
